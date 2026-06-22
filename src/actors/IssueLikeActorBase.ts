8c13fc2e80456a534268acb89fce60918e50002a
actionchain <actionchain@chainguard.dev>
2026-06-22 21:20:15 +0000

    Update octoguide/bot@0.21.8

---
+++ src/actors/IssueLikeActorBase.ts
@@ -0,0 +1,42 @@
+import type { CommentData, IssueLikeData } from "../types/entities.js";
+
+import { EntityActorBase } from "./EntityActorBase.js";
+
+export abstract class IssueLikeActorBase<
+	Data extends CommentData | IssueLikeData,
+> extends EntityActorBase<Data> {
+	async createComment(body: string) {
+		const response = await this.octokit.rest.issues.createComment({
+			body,
+			issue_number: this.entityNumber,
+			owner: this.locator.owner,
+			repo: this.locator.repository,
+		});
+
+		return response.data.html_url;
+	}
+
+	abstract getData(): Promise<Data>;
+
+	async listComments() {
+		// TODO: Retrieve all pages, not just the first one
+		// https://github.com/OctoGuide/bot/issues/34
+		const comments = await this.octokit.rest.issues.listComments({
+			issue_number: this.entityNumber,
+			owner: this.locator.owner,
+			per_page: 100,
+			repo: this.locator.repository,
+		});
+
+		return comments.data;
+	}
+
+	async updateComment(number: number, newBody: string) {
+		await this.octokit.rest.issues.updateComment({
+			body: newBody,
+			comment_id: number,
+			owner: this.locator.owner,
+			repo: this.locator.repository,
+		});
+	}
+}
